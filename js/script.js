86e77be6ddf0730c2933cc7640367a62fcdeabaf
Pedro Henrique Godri <pedrogodri@gmail.com>
2022-12-12 22:33:10 -0300

    Scripts adicionando - JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-const
+const input = document.querySelector('input');
+const qrCode = document.querySelector('#qrCode');
+
+// Eventos
+document.addEventListener('keypress', (e) => {
+    if(e.key === 'Enter') {
+        gerarQRCode();
+    }
+});
+
+// Funções
+const gerarQRCode = () => {
+    if(!input.value) return;
+
+    qrCode.src = `https://api.server.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
+}
